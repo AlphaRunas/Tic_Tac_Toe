50f5308d9d6ed8b2aa104ce5c6df7c517e1bc1ef
Roland <rfisher4444@hotmail.com>
2024-07-26 19:06:00 -0700

    Added a computerPlayer class that will hold all the logic for the computer when playing against one player

---
+++ computerPlayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Roland Fisher on 7/26/24.
+//
+
+#include "computerPlayer.h"
---
+++ computerPlayer.h
@@ -0,0 +1,17 @@
+//
+// Created by Roland Fisher on 7/26/24.
+//
+
+#ifndef TIC_TAC_TOE_COMPUTERPLAYER_H
+#define TIC_TAC_TOE_COMPUTERPLAYER_H
+
+// Will hold prompts and decisions that the computer will make
+// when playing against the player if there is only one human
+// player.
+
+class computerPlayer {
+
+};
+
+
+#endif //TIC_TAC_TOE_COMPUTERPLAYER_H
