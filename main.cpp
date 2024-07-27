c704f5708fb77c1a4a6013bb9c635b22c664494b
Roland <rfisher4444@hotmail.com>
2024-07-26 19:00:21 -0700

    Added some comments

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
+// Tic-Tac-Toe game where you can play either vs the computer or another person
+// Roland Fisher
+// Start date: 7-26-24
+//
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
